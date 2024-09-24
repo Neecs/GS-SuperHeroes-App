6a358bd3df4c4f306c827eeb2b6343ba8134d412
Nicolas Gil <nikosgil12@gmail.com>
2024-09-24 17:24:05 -0500

    DataHelper duplication removed

--- app/src/main/java/com/example/superheroapp/MainActivity.kt
+++ app/src/main/java/com/example/superheroapp/MainActivity.kt
@@ -1,16 +1,24 @@
+// MainActivity.kt
 package com.example.superheroapp
 
 import android.os.Bundle
-import android.util.Log
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.superheroapp.data.generateSuperheroes
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        // Genera la lista de superhéroes una sola vez
+        val superheroes = generateSuperheroes()
+        val adapter = SuperheroAdapter(superheroes)
+        recyclerView.adapter = adapter
     }
 }
