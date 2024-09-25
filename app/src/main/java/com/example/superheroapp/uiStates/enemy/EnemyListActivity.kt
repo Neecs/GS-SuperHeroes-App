53c5e2e6eb38c91180dd65fc5423c5019f0a3caf
SergioSoto19 <ser192000gio@gmail.com>
2024-09-25 14:14:30 -0500

    impelentacion de recyclerViewenemigos sin  ViewModel

--- app/src/main/java/com/example/superheroapp/uiStates/enemy/EnemyListActivity.kt
+++ app/src/main/java/com/example/superheroapp/uiStates/enemy/EnemyListActivity.kt
@@ -1,26 +1,18 @@
-package com.example.superheroapp
+package com.example.superheroapp.uiStates.enemy
 
 import android.os.Bundle
-import android.util.Log
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.superheroapp.R
 import com.example.superheroapp.adapters.EnemyAdapter
 import com.example.superheroapp.data.generateEnemies
-import com.example.superheroapp.data.generateSuperheroes
 
-class MainActivity : AppCompatActivity() {
+class EnemyListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
-
-
-
-
         setContentView(R.layout.rv_enemy)
+
         val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = EnemyAdapter(generateEnemies())
